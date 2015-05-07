371c0ba0b4f2157e6ce8181b505f55a5fca4fc8c
JcBernack <jc.bernack@gmail.com>
2015-05-07 13:47:11 +0200

    Fix button and window state bug when the editor is left while one of the windows is still open

--- CraftShare/CraftListMod.cs
+++ CraftShare/CraftListMod.cs
@@ -8,6 +8,8 @@ namespace CraftShare
     {
         private const ApplicationLauncher.AppScenes VisibleInScenes = ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.VAB;
 
+        public ApplicationLauncherButton AppLauncherButton;
+
         public void Awake()
         {
             // initialize globally used objects
@@ -17,15 +19,25 @@ namespace CraftShare
             else GameEvents.onGUIApplicationLauncherReady.Add(AddLauncherButton);
         }
 
-        private void AddLauncherButton()
-        {
-            ApplicationLauncher.Instance.AddModApplication(ModGlobals.MainWindow.Open, ModGlobals.MainWindow.Close, null, null, null, null, VisibleInScenes, ModGlobals.IconSmall);
-            GameEvents.onGUIApplicationLauncherReady.Remove(AddLauncherButton);
-        }
-
         public void Start()
         {
             DontDestroyOnLoad(this);
+        }
+
+        private void AddLauncherButton()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Remove(AddLauncherButton);
+            AppLauncherButton = ApplicationLauncher.Instance.AddModApplication(ModGlobals.MainWindow.Open, ModGlobals.MainWindow.Close,
+                null, null, null, OnDisable, VisibleInScenes, ModGlobals.IconSmall);
+        }
+
+        private void OnDisable()
+        {
+            // make sure the windows are properly closed
+            ModGlobals.MainWindow.Close();
+            ModGlobals.SettingsWindow.Close();
+            // and the button is in its "false" state
+            if (AppLauncherButton != null) AppLauncherButton.SetFalse();
         }
     }
 }
